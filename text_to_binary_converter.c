bd39301f84578f68aee0421f662116e0b47ef528
l-kaushik <contact.lokeshkaushik@gmail.com>
2022-12-07 20:44:18 +0530

    Initial commit

---
+++ text_to_binary_converter.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+void deciToBi(int decimal)
+{
+    int deci = decimal, i, k, temp[8], result = 0;
+    long int pten = 10000000;
+
+    if (deci <= 255)
+    {
+        for (i = 0; i < 8; i++)
+        {
+            temp[i] = deci % 2;
+            deci = deci / 2;
+        }
+        for (i = 7; i >= 0; i--)
+        {
+            printf("%d", temp[i]);
+        }
+    }
+}
+
+void main()
+{
+    char input[50];
+    int len;
+
+    printf("Enter your text:- ");
+    len = strlen(gets(input));
+
+    for (int i = 0; i < len; i++)
+    {
+        int num = 0;
+
+        num = input[i];
+        deciToBi(num);
+        printf(" ");
+    }
+}
